71f9bae9521adf6bdb130d29257e6a5b75c77a9b
evnnn16-netizen <evnnn16@gmail.com>
2026-02-20 18:54:18 +0100

    Delete CODE directory

--- CODE/tirage-au-sort/tirage-au-sort.js
+++
@@ -1,9 +0,0 @@
-let mangas = ["Naruto", "One Piece", "DBZ", "Bleach", "HxH", "JJK", "Spy x Family", "Sakamoto Days", "JJBA", "HnK", "Boruto"];
-
-function tirage() {
-    let indexAleatoire = Math.floor(Math.random() * mangas.length);
-
-    let mangaChoisi = mangas[indexAleatoire];
-
-    document.getElementById("affichage").textContent = "Le manga tiré au sort est : " + mangaChoisi;
-}
